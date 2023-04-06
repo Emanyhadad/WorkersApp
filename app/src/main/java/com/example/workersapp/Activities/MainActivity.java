3db21d0952bfc42bc91348b51b6e7ed1b0e5dfbb
admin <arwashamaly311@gmail.com>
2023-04-06 22:32:08 +0300

    enable authentication phone LoginActivity

--- app/src/main/java/com/example/workersapp/Activities/MainActivity.java
+++ app/src/main/java/com/example/workersapp/Activities/MainActivity.java
@@ -1,19 +1,57 @@
 package com.example.workersapp.Activities;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.workersapp.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-ActivityMainBinding binding;
+    ActivityMainBinding binding;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+
+
     @Override
-    protected void onCreate( Bundle savedInstanceState ) {
-        super.onCreate( savedInstanceState );
-        binding=ActivityMainBinding.inflate(getLayoutInflater());
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+
+        binding.imageView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Logout")
+                        .setMessage("You are about to log out of your account!")
+                        .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        })
+                        .setPositiveButton("logout", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                firebaseAuth.signOut();
+
+                                firebaseAuth = FirebaseAuth.getInstance();
+
+                                finish();
+                            }
+                        })
+                        .create().show();
+            }
+        });
 
     }
 }
