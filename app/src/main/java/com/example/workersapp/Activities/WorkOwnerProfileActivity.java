0997d369cda4b6a6c319e878b17300fde05c39b8
admin <arwashamaly311@gmail.com>
2023-05-11 19:12:24 +0300

    create WorkInProgressFragment and item_finished_jobs.xml

--- app/src/main/java/com/example/workersapp/Activities/WorkOwnerProfileActivity.java
+++ app/src/main/java/com/example/workersapp/Activities/WorkOwnerProfileActivity.java
@@ -1,12 +1,17 @@
 package com.example.workersapp.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.workersapp.Fragments.NewJobFragment;
+import com.example.workersapp.Fragments.WorkOwnerProfileFragment;
+import com.example.workersapp.Fragments.WorkInProgressFragment;
 import com.example.workersapp.R;
 import com.example.workersapp.databinding.ActivityWorkOwnerProfileBinding;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class WorkOwnerProfileActivity extends AppCompatActivity {
     ActivityWorkOwnerProfileBinding binding;
@@ -17,7 +22,31 @@ public class WorkOwnerProfileActivity extends AppCompatActivity {
         binding = ActivityWorkOwnerProfileBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        getSupportFragmentManager().beginTransaction().add(R.id.container, new NewJobFragment()).commit();
-
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new WorkOwnerProfileFragment()).commit();
+        NavigationBarView.OnItemSelectedListener listener = new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.itProfile:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, new WorkOwnerProfileFragment()).commit();
+                        return true;
+                    case R.id.itInProgress:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, new WorkInProgressFragment()).commit();
+                        return true;
+                    case R.id.itOpenJobs:
+                        // add code for itOpenJobs case here
+                        return true;
+                    case R.id.itNewJob:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, new NewJobFragment()).commit();
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        };
+        // تنشيط itProfile
+        binding.workOwnerNav.getMenu().findItem(R.id.itProfile).setChecked(true);
+        binding.workOwnerNav.getMenu().performIdentifierAction(R.id.itProfile, 0);
+        binding.workOwnerNav.setOnItemSelectedListener(listener);
     }
 }
